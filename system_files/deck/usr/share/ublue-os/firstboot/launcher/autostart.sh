2bd020bd65dabb6063f6c604d637197f01dcceb7
RJ Trujillo <eyecantcu@pm.me>
2023-06-29 18:14:23 -0600

    feat(deck): Hook Steam into autostart script

---
+++ system_files/deck/usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Simply launches the "yafti" GUI with the uBlue image's configuration.
+/usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
+
+# Open Steam so that it can update
+/usr/bin/steam
