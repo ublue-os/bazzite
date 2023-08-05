b3ed366dcf60cd14e355fc45b7690a2e65e2a863
RJ Trujillo <eyecantcu@pm.me>
2023-08-04 22:20:59 -0600

    fix(yafti): Revert to using grep and remove direct usage of image identifiers
    Sourcing these doesn't seem to cut it as yafti doesn't acknowledge them so instead just use
    cat to obtain the file contents for the same result

--- system_files/deck/shared/usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ system_files/deck/shared/usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Source Bazzite defaults
-source /etc/default/bazzite
-
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
 
