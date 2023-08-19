73f267c4916594a6712f70e757d3cd612dbb310f
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-08-19 00:03:51 -0700

    fix: Use system level alias for Fish shell

---
+++ system_files/desktop/shared/usr/share/fish/vendor_conf.d/neofetch.fish
@@ -0,0 +1,2 @@
+alias neofetch='/usr/bin/neowofetch --source /usr/share/ublue-os/bazzite/logo.txt --config /usr/share/ublue-os/bazzite/neofetch.conf'
+alias neowofetch='/usr/bin/neowofetch --source /usr/share/ublue-os/bazzite/logo.txt --config /usr/share/ublue-os/bazzite/neofetch.conf'
