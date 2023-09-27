3621027fdff170a43970045365095d0d4bc8f62f
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-09-27 15:47:13 -0700

    fix: Correct issue where bazzite-arch updater fails if you're using a bazzite-arch image at a non-default name

--- system_files/desktop/shared/usr/etc/ublue-update.d/user/99-bazzite-arch-user-update.sh
+++ system_files/desktop/shared/usr/etc/ublue-update.d/user/99-bazzite-arch-user-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [[ $(distrobox list | grep bazzite-arch) ]]; then
+if [[ $(podman ps -a --no-trunc --format {{.Names}} | grep -E '(^|\s)bazzite-arch($|\s)') ]]; then
 	/usr/bin/distrobox-enter -n bazzite-arch -- '  paru -Sua --noconfirm'
 else
 	echo "Update skipped: bazzite-arch not installed"
