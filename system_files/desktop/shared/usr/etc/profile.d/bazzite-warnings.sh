0beec60feed0895acba164d09983984713f5b1bb
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-09-30 08:44:09 -0700

    chore: Simplify nvidia check

--- system_files/desktop/shared/usr/etc/profile.d/bazzite-warnings.sh
+++ system_files/desktop/shared/usr/etc/profile.d/bazzite-warnings.sh
@@ -2,7 +2,7 @@ IMAGE_INFO="/usr/share/ublue-os/image-info.json"
 IMAGE_FLAVOR=$(jq -r '."image-flavor"' < $IMAGE_INFO)
 
 if [[ $IMAGE_FLAVOR = "nvidia"  ]]; then
-	if ! grep -q "nvidia" <<< $(lsmod) || grep -q "nouveau" <<< $(lsmod); then
+	if ! grep -q "nvidia" <<< $(lsmod); then
 		echo -e 'You are using a Nvidia image, but the Nvidia driver is not loaded:\n - If you are using secure boot, run "just enroll-secure-boot-key", then reboot and enter the password "ublue-os" when prompted.\n - If you are not using secure boot or have already enrolled the above key, ensure you have the needed kargs by running "just nvidia-set-kargs" and then rebooting.\n - This message will not appear if the issue is resolved.\n'
 	fi
 else
