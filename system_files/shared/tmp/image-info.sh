8d67292ae1894868fd8768d0e3a3714d8ada2251
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-01-05 17:59:55 -0800

    fix: Set up bazzite-hardware-setup to re-run if the branch changes

--- system_files/shared/tmp/image-info.sh
+++ system_files/shared/tmp/image-info.sh
@@ -21,6 +21,7 @@ cat > $IMAGE_INFO <<EOF
   "image-vendor": "$IMAGE_VENDOR",
   "image-ref": "$IMAGE_REF",
   "image-tag": "$IMAGE_TAG",
+  "image-branch": "$IMAGE_BRANCH",
   "base-image-name": "$BASE_IMAGE_NAME",
   "fedora-version": "$FEDORA_MAJOR_VERSION"
 }
