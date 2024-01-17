1fa350df2e6b33e7435ddef5ff5b20acef674c05
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-01-17 13:31:31 -0800

    chore: Update nvidia karg message

--- system_files/desktop/shared/usr/etc/profile.d/bazzite-warnings.sh
+++ system_files/desktop/shared/usr/etc/profile.d/bazzite-warnings.sh
@@ -3,7 +3,7 @@ IMAGE_FLAVOR=$(jq -r '."image-flavor"' < $IMAGE_INFO)
 
 if [[ $IMAGE_FLAVOR = "nvidia"  ]]; then
 	if ! grep -q "nvidia" <<< $(lsmod); then
-		echo -e 'You are using a Nvidia image, but the Nvidia driver is not loaded:\n - If you are using secure boot, run "ujust enroll-secure-boot-key", then reboot and enter the password "ublue-os" when prompted.\n - If you are not using secure boot or have already enrolled the above key, ensure you have the needed kargs by running "ujust nvidia-set-kargs" and then rebooting.\n - This message will not appear if the issue is resolved.\n'
+		echo -e 'You are using a Nvidia image, but the Nvidia driver is not loaded:\n - If you are using secure boot, run "ujust enroll-secure-boot-key", then reboot and enter the password "ublue-os" when prompted.\n - If you are not using secure boot or have already enrolled the above key, ensure you have the needed kargs by running "ujust configure-nvidia kargs" and then rebooting.\n - This message will not appear if the issue is resolved.\n'
 	fi
 else
 	if ! grep -q "v4l2loopback" <<< $(lsmod); then
