8730ef103939bb425e4989ecab83c0e04ad3c2df
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-01-18 00:12:41 -0800

    chore: Remove secure boot warning

--- system_files/desktop/shared/usr/etc/profile.d/secure-boot-warning.sh
+++
@@ -1,1 +0,0 @@
-echo -e 'Warning: The next major update for Bazzite will introduce new requirements for secure boot to work.\nPlease make sure to run "ujust enroll-secure-boot-key" at least once.\nThis message will automatically disappear after the update has been deployed.\nFor more information, see our thread at: https://universal-blue.discourse.group/t/secure-boot-notice/405\n'
