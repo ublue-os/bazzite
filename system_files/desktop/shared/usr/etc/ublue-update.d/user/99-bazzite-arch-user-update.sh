97aeca6e1ba0b5a703f564e4dbae7d976013ce40
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-01-20 01:02:39 -0800

    feat: Use topgrade for ujust update

--- system_files/desktop/shared/usr/etc/ublue-update.d/user/99-bazzite-arch-user-update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-if [[ $(podman ps -a --no-trunc --format {{.Names}} | grep -E '(^|\s)bazzite-arch($|\s)') ]]; then
-	unset SUDO_USER # avoid distrobox sudo checks
-	/usr/bin/distrobox-enter -n bazzite-arch -- '  paru -Sua --noconfirm'
-else
-	echo "Update skipped: bazzite-arch not installed"
-fi
