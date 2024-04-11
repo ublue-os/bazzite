a2ada58e68e39ccc585fee22daefc7b1e6e33f64
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-04-11 16:28:14 -0700

    chore: Populate KERNEL_SUFFIX

--- scripts/initramfs.sh
+++ scripts/initramfs.sh
@@ -2,4 +2,10 @@
 
 set -oue pipefail
 
+if [[ "${AKMODS_FLAVOR}" == "surface" ]]; then
+    KERNEL_SUFFIX="surface"
+else
+    KERNEL_SUFFIX=""
+fi
+
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')" --reproducible -v --add ostree -f /tmp/dracut
