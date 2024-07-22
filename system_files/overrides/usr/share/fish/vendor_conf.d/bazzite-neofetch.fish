fbf6de47b1351183dfcbcbb0d8cabe1eb325f600
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-07-21 23:31:21 -0700

    chore: Clean up bazzite-neofetch.fish

--- system_files/overrides/usr/share/fish/vendor_conf.d/bazzite-neofetch.fish
+++ system_files/overrides/usr/share/fish/vendor_conf.d/bazzite-neofetch.fish
@@ -1,4 +1,4 @@
-alias neofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo-color-1 94 --logo-color-2 47 --logo-color-3 95 --logo-color-4 0 --logo-color-5 8 --logo-color-6 55'
-alias neowofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo-color-1 94 --logo-color-2 47 --logo-color-3 95 --logo-color-4 0 --logo-color-5 8 --logo-color-6 55'
-alias hyfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo-color-1 94 --logo-color-2 47 --logo-color-3 95 --logo-color-4 0 --logo-color-5 8 --logo-color-6 55'
-alias fastfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo-color-1 94 --logo-color-2 47 --logo-color-3 95 --logo-color-4 0 --logo-color-5 8 --logo-color-6 55'
+alias neofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
+alias hyfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
