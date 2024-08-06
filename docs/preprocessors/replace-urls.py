a6bf1f3aec4ccad4a7eabaf70de1eacfdd624b3b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-08-06 19:18:40 +0200

    docs: Add 'replace-urls' preprocessor

---
+++ docs/preprocessors/replace-urls.py
@@ -0,0 +1,57 @@
+__doc__ = """Replace urls across the entire book"""
+
+import json
+import sys
+
+from urllib.parse import urlparse
+
+from libs.utils import Utils, debug as _debug
+
+
+def debug(*obj):
+    return _debug("REPLACE-URLS:", *obj)
+
+
+_IGNORE_STRINGS = [
+    "before",
+    "after",
+    "command",
+    "renderers",
+]
+
+
+def is_url(url) -> bool:
+    res: bool = False
+    try:
+        tmp = urlparse(url)
+        res = tmp.netloc != "" and tmp.scheme != ""
+    except Exception as _:
+        res = False
+    return res
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "supports":
+            sys.exit(0)
+    context, book = json.load(sys.stdin)
+    book: dict[str, list]
+
+    config = Utils.get_config_from_ctx("replace-urls", context)
+    if not config:
+        print(json.dumps(book))
+        exit(0)
+    debug(config)
+
+    # Get the url mappings
+    url_mappings: list[tuple[str, str]] = [
+        (k, v) for k, v in config.items() if k not in _IGNORE_STRINGS and is_url(k)
+    ]
+    debug(url_mappings)
+
+    book_s = json.dumps(book)
+
+    for mapp_old, map_new in url_mappings:
+        book_s = book_s.replace(mapp_old, map_new)
+
+    print(book_s)
