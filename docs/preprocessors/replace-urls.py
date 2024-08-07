c27ac011badfa65d1ede303bf4348fc25f57647b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-08-07 19:22:23 +0200

    docs: Add ignore field to replace-urls
    Now we can exclude files from being processed with blob patterns

--- docs/preprocessors/replace-urls.py
+++ docs/preprocessors/replace-urls.py
@@ -1,16 +1,16 @@
 __doc__ = """Replace urls across the entire book"""
 
-from dataclasses import asdict, dataclass
-from functools import singledispatch
-from glob import glob
+from copy import copy
+import glob
 import json
 from pathlib import Path
 import sys
 
-from typing import Self, cast, overload
+from typing import cast
 from urllib.parse import urlparse
 
 from libs.utils import Utils, debug as _debug
+from libs.types import MdBook
 
 
 def debug(*obj):
@@ -23,49 +23,6 @@ _IGNORE_STRINGS = [
     "command",
     "renderers",
 ]
-
-
-@dataclass
-class Chapter(dict):
-    name: str
-    content: str
-    number: int
-    path: str
-    source_path: str
-    parent_names: list[str]
-
-    def __init__(self, name, content, number, path, source_path, parent_names):
-        self.name = name
-        self.content = content
-        self.number = number
-        self.path = path
-        self.source_path = source_path
-        self.parent_names = parent_names
-        self._ref = dict()
-
-    @classmethod
-    def from_dict(cls, d: dict) -> Self:
-        inst = cls(
-            name=d["name"],
-            content=d["content"],
-            number=d["number"],
-            path=d["path"],
-            source_path=d["source_path"],
-            parent_names=d["parent_names"],
-        )
-        inst._ref = d
-        return inst
-
-
-class Chap(object):
-    def __init__(self, d) -> None:
-        self.__dict__ = d
-        self.name: str = self.__dict__["name"]
-        self.content: str = self.__dict__["content"]
-        self.number: int = self.__dict__["number"]
-        self.path: str = self.__dict__["path"]
-        self.source_path: str = self.__dict__["source_path"]
-        self.parent_names: list[str] = self.__dict__["parent_names"]
 
 
 def is_url(url) -> bool:
@@ -83,18 +40,24 @@ if __name__ == "__main__":
         if sys.argv[1] == "supports":
             sys.exit(0)
     context, book = json.load(sys.stdin)
-    book: dict[str, list]
+
+    book = MdBook(book)
 
     config = Utils.get_config_from_ctx("replace-urls", context)
     if not config:
-        print(json.dumps(book))
+        print(json.dumps(book._data))
         exit(0)
     elif not isinstance(config, dict):
-        print(json.dumps(book))
+        print(json.dumps(book._data))
         exit(0)
 
-    ignore_paths = config.get("ignore") or []
-    debug(config)
+    ignore_paths_list_globs = cast(list[str], list(config.get("ignore") or []))
+    ignore_paths: list[str] = []
+    root_dir = Path(context["root"], context["config"]["book"]["src"])
+    for p in ignore_paths_list_globs:
+        ignore_paths += glob.glob(p, root_dir=root_dir)
+
+    debug("My ignored paths:", ignore_paths)
 
     config_mappings: dict = config["mappings"]
 
@@ -110,21 +73,18 @@ if __name__ == "__main__":
     # for mapp_old, map_new in url_mappings:
     #     book_s = book_s.replace(mapp_old, map_new)
 
-    book_aux: dict[str, list[dict]] = book.copy()
-
-    for idx, section in enumerate(book["sections"].copy()):
-        section: dict
-        if not section.get("Chapter"):
+    for section in book.sections:
+        if not section.chapter:
+            debug("Section skipped, was parttitle:", section.part_title)
             continue
 
-        chapter = Chap(section["Chapter"])
-
-        if chapter.path in ignore_paths:
+        debug("section.chapter.path =", section.chapter.path)
+        if section.chapter.path in ignore_paths:
+            debug("Section skipped, was in ignore_paths:", section.chapter.path)
             continue
 
         for old_url, new_url in url_mappings:
-            chapter.content.replace(old_url, new_url)
-        book_aux["sections"][idx] = section
+            old = copy(section.chapter.content)
+            section.chapter.content = section.chapter.content.replace(old_url, new_url)
 
-    book.update(book_aux)
-    print(json.dumps(book))
+    print(json.dumps(book._data))
