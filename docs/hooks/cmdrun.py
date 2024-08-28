b6078f7b8878d6ba99c26015f066c18a169a30ce
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-08-28 16:38:40 +0200

    docs: add more url replacements

--- docs/hooks/cmdrun.py
+++ docs/hooks/cmdrun.py
@@ -13,11 +13,67 @@ import re
 from fetch_discourse_md import fetch as fetch_md_discourse
 
 
+########################### ONLY MODIFY THIS ###########################
 URL_MAPPINGS = [
+    # region GENERAL
     (  # src/index.md
         "https://universal-blue.discourse.group/docs?topic=561",
         "/",
     ),
+    (  # src/General/reporting_bugs.md
+        "https://universal-blue.discourse.group/docs?topic=3402",
+        "/General/reporting_bugs",
+    ),
+    # region INSTALLATION GUIDE
+    (  # src/General/Installation_Guide/Installing_Bazzite_for_HTPC_Setups.md
+        "https://universal-blue.discourse.group/docs?topic=1145",
+        "/General/Installation_Guide/Installing_Bazzite_for_HTPC_Setups/",
+    ),
+    (  # src/General/Installation_Guide/index.md
+        "https://universal-blue.discourse.group/docs?topic=35",
+        "/Installing_and_Managing_Software/",
+    ),
+    (  # src/General/Installation_Guide/dual_boot_setup_guide.md
+        "https://universal-blue.discourse.group/docs?topic=2743",
+        "/General/Installation_Guide/dual_boot_setup_guide/",
+    ),
+    (  # src/General/Installation_Guide/secure_boot.md
+        "https://universal-blue.discourse.group/docs?topic=2742",
+        "/General/Installation_Guide/secure_boot",
+    ),
+    (  # src/General/Installation_Guide/troubleshoot_guide.md
+        "https://universal-blue.discourse.group/docs?topic=2495",
+        "/General/Installation_Guide/troubleshoot_guide/",
+    ),
+    # endregion INSTALLATION GUIDE
+    # endregion GENERAL
+    # region SOFTWARE
+    (  # src/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/index.md
+        "https://universal-blue.discourse.group/docs?topic=36",
+        "/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/",
+    ),
+    (  # src/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/updating_guide.md
+        "https://universal-blue.discourse.group/docs?topic=2637",
+        "/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/updating_guide/",
+    ),
+    (  # src/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/rolling_back_system_updates.md
+        "https://universal-blue.discourse.group/docs?topic=2644",
+        "/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/rolling_back_system_updates/",
+    ),
+    (  # src/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/rebase_guide.md
+        "https://universal-blue.discourse.group/docs?topic=2646",
+        "/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/rebase_guide/",
+    ),
+    (  # src/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/bazzite_rollback_helper.md
+        "https://universal-blue.discourse.group/docs?topic=2647",
+        "/Installing_and_Managing_Software/Updates_Rollbacks_&_Rebasing/bazzite_rollback_helper/",
+    ),
+    # endregion SOFTWARE
+    (  # src/Gaming/index.md
+        "https://universal-blue.discourse.group/docs?topic=31",
+        "/Gaming/",
+    ),
+    # region HTPC
     (  # src/Handheld_and_HTPC_edition/Handheld_Wiki/index.md
         "https://universal-blue.discourse.group/docs?topic=1038",
         "/Handheld_and_HTPC_edition/Handheld_Wiki/",
@@ -26,15 +82,21 @@ URL_MAPPINGS = [
         "https://universal-blue.discourse.group/docs?topic=1849",
         "/Handheld_and_HTPC_edition/Handheld_Wiki/Steam_Deck/",
     ),
-    (  # src/Handheld_and_HTPC_edition/Steam_Gaming_Mode/index.md
+    (  # src/Handheld_and_HTPC_edition/Steam_Gaming_Mode.md
         "https://universal-blue.discourse.group/docs?topic=37",
         "/Handheld_and_HTPC_edition/Steam_Gaming_Mode/",
     ),
-    (  # src/General/Installation_Guide/dual_boot_setup_guide.md
-        "https://universal-blue.discourse.group/docs?topic=2743",
-        "/General/Installation_Guide/dual_boot_setup_guide/",
+    # endregion HTPC
+    # region ADVANCED
+    (
+        # src/Advanced/Auto-Mounting_Secondary_Drives.md
+        "https://universal-blue.discourse.group/docs?topic=970",
+        "/Advanced/Auto-Mounting_Secondary_Drives/",
     ),
+    # endregion ADVANCED
 ]
+
+########################################################################
 
 
 CMDRUN_PATTERN = r"<!--\s?cmdrun\s+fetch_discourse_md\.py\s+\"(.*)\"\s*-->"
