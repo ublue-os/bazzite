f5a352a1c0b2a0e2a1780bb37f0eca86677db340
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-01-02 13:55:16 -0800

    chore: Disable webspeech in firefox by default

--- system_files/desktop/shared/usr/share/ublue-os/firefox-config/01-bazzite-global.js
+++ system_files/desktop/shared/usr/share/ublue-os/firefox-config/01-bazzite-global.js
@@ -1,3 +1,5 @@
 // Bazzite Global
 pref("gfx.webrender.all", true);
 pref("media.ffmpeg.vaapi.enabled", true);
+pref("reader.parse-on-load.enabled", false);
+pref("media.webspeech.synth.enabled", false);
