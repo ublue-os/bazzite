ba751021c146bbe814e2b48c827e629863256cea
Pat Connors <121328689+nicknamenamenick@users.noreply.github.com>
2025-04-13 15:36:15 -0400

    chore: Remove hyfetch aliases since there's a Brew package for this now (#2477)
    * Remove hyfetch alias from bash
    
    * Remove hyfetch alias from fish

--- system_files/desktop/shared/etc/profile.d/bazzite-neofetch.sh
+++ system_files/desktop/shared/etc/profile.d/bazzite-neofetch.sh
@@ -1,4 +1,3 @@
 alias neofetch='/usr/bin/fastfetch --color $(/usr/libexec/bazzite-bling-fastfetch) -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
 alias neowofetch='/usr/bin/fastfetch --color $(/usr/libexec/bazzite-bling-fastfetch) -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
-alias hyfetch='/usr/bin/fastfetch --color $(/usr/libexec/bazzite-bling-fastfetch) -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
 alias fastfetch='/usr/bin/fastfetch --color $(/usr/libexec/bazzite-bling-fastfetch) -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
