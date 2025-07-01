579194a77a0d60134f649e1af0ff47d61425e48b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-07-01 20:58:53 +0200

    fix: Copy in fedora welcome icon instead of symlinking
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- installer/titanoboa_hook_postrootfs.sh
+++ installer/titanoboa_hook_postrootfs.sh
@@ -313,7 +313,7 @@ fi
 # Tweak the fedora-welcome app (gnome only) with our own text/icons
 if [[ $desktop_env == gnome ]]; then
     sed -i 's| Fedora| Bazzite|' /usr/share/anaconda/gnome/fedora-welcome || :
-    ln -sf fedora-logo-sprite.png /usr/share/pixmaps/fedora-logo-icon.png
+    cp -f /usr/share/pixmaps/{fedora-logo-sprite,fedora-logo-icon}.png || :
 fi
 
 ###############################
