fa693a87bf0a65d74e1c245d42ef6adfb7434042
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-07-04 00:45:41 -0700

    chore: Drop some no longer needed firefox params

--- system_files/desktop/shared/usr/share/ublue-os/firefox-config/01-bazzite-global.js
+++ system_files/desktop/shared/usr/share/ublue-os/firefox-config/01-bazzite-global.js
@@ -1,5 +1,4 @@
 // Bazzite Global
-pref("gfx.webrender.all", true);
 pref("media.ffmpeg.vaapi.enabled", true);
 pref("reader.parse-on-load.enabled", false);
 pref("media.webspeech.synth.enabled", false);
