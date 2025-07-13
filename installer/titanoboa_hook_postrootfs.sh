1386c967336d97f70fc9ebd8e8e9b47025f422e8
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-07-13 17:23:21 +0200

    feat(liveiso): Have installer and browser in the app dock
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- installer/titanoboa_hook_postrootfs.sh
+++ installer/titanoboa_hook_postrootfs.sh
@@ -401,4 +401,17 @@ if [[ $desktop_env == gnome ]]; then
     cp -f /usr/share/pixmaps/{fedora-logo-sprite,fedora-logo-icon}.png || :
 fi
 
+# Let only browser/installer in the task-bar/dock
+if [[ $desktop_env == kde ]]; then
+    sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:anaconda.desktop,preferred:\/\/filemanager<\/default>/' \
+        /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+elif [[ $desktop_env == gnome ]]; then
+    cat >/usr/share/glib-2.0/schemas/zz2-org.gnome.shell.gschema.override <<EOF
+[org.gnome.shell]
+welcome-dialog-last-shown-version='4294967295'
+favorite-apps = ['anaconda.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop']
+EOF
+    glib-compile-schemas /usr/share/glib-2.0/schemas
+fi
+
 ###############################
