aa86b9701e798545fa67b91ab5dad59b6df2aaa0
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-07-17 19:39:30 +0200

    fix(liveiso): set grub var manually
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- installer/titanoboa_hook_preinitramfs.sh
+++ installer/titanoboa_hook_preinitramfs.sh
@@ -19,8 +19,14 @@ dnf -y versionlock delete "${kernel_pkgs[@]}"
 rpm --erase -v --nodeps "${kernel_pkgs[@]}"
 dnf -yq install "${kernel_pkgs[@]}"
 
-# Hide grub by default
-grub2-editenv - set menu_auto_hide=2
+# Hide grub by default and set timeout to 5 seconds
+mkdir -p /boot/grub2
+cat >>/boot/grub2/grub.cfg <<'EOF'
 
-# Set timeout to 5 seconds
+# Setup for liveisos
+set menu_auto_hide=2
+set timeout_style=hidden
+set timeout=5
+EOF
+
 sed -i 's|GRUB_TIMEOUT=[0-9]*$|GRUB_TIMEOUT=5|' /etc/default/grub
