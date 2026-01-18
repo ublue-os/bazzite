66501d9913742c9c3b56fee6ed835240cae81ed8
stellaberrant <104581805+stellaberrant@users.noreply.github.com>
2026-01-18 16:14:59 +0100

    feat: ntfs/exfat notification service
    improved the monitoring script with less overhead, add 'temporarily ignore' functionality, put text into variables

--- system_files/desktop/shared/usr/libexec/ntfs_exfat_monitor_script.sh
+++ system_files/desktop/shared/usr/libexec/ntfs_exfat_monitor_script.sh
@@ -1,42 +1,52 @@
 #!/usr/bin/env bash
 
-#set initial count variables
+#set user facing variables
+firstbutton="I understand"
+firstbutton_ignore="Temporarily ignore"
+secondbutton="Learn more"
+sender="Bazzite"
+header="Unsupported filesystem"
+warning="You have mounted an NTFS/exFAT (Windows) partition. You can use it for storing data, however running games from Windows drives will cause problems."
+documentation="https://docs.bazzite.gg/Gaming/Hardware_compatibility_for_gaming/#unsupported-filesystems-for-secondary-drives"
+
+#set other variables
+counter=0
 ntfs=$(lsblk -o fstype,mountpoint | grep -Ec 'ntfs.*\S+')
 exfat=$(lsblk -o fstype,mountpoint | grep -Ec 'exfat.*\S+')
 
-#monitor for device add/remove events
-udevadm monitor --kernel --udev --subsystem=block | while read -r line; do
-
-#reduce # of greps
-    if [[ "$line" != *" change "* ]]; then
-        continue
-    fi
-
+#monitor for fuseblk and exfat mount events
+findmnt -n --poll -t exfat,ntfs,fuseblk | while read -r _; do
 
     new_ntfs=$(lsblk -o fstype,mountpoint | grep -Ec 'ntfs.*\S+')
     new_exfat=$(lsblk -o fstype,mountpoint | grep -Ec 'exfat.*\S+')
     #compare current count to last count, if number increased,
     if [[ "$new_ntfs" > "$ntfs" ]] || [[ "$new_exfat" > "$exfat" ]];
         then
-            echo "ntfs count "$ntfs
-            echo "new ntfs count "$new_ntfs
-            echo "exfat count "$exfat
-            echo "new exfat count "$new_exfat
+            echo "ntfs count: "$ntfs "-> new ntfs count: "$new_ntfs
+            echo "exfat count: "$exfat "-> new exfat count: "$new_exfat
+            #change button text if first button was clicked 5 times or more
+            if [[ "$counter" > 4 ]]; then
+                firstbutton=$firstbutton_ignore
+            fi
             # send notification
-            choice=$(notify-send  -t 500000 --action="opt1=I understand" --action="opt2=Learn more"  -a "Bazzite" "Unsupported filesystems"  "You have mounted an NTFS/exFAT (Windows) partition. You can use it for storing data, however running games from Windows drives is unsupported.")
-            echo "User selected $choice"
+            choice=$(notify-send  -t 500000 --action="opt1=$firstbutton" --action="opt2=$secondbutton"  -a "$sender" "$header" "$warning")
                 case $choice in
                     "opt1")
-                        #do nothing
+                        if [[ "$counter" > 4 ]]; then
+                            echo "stopping service as per user choice. Goodbye."
+                            systemctl --user stop ntfs
+                        fi
+                        ((counter++))
+                        echo "counter: "$counter
                         ;;
                     "opt2")
                         #open documentation in browser
-                        xdg-open "https://docs.bazzite.gg/Gaming/Hardware_compatibility_for_gaming/#unsupported-filesystems-for-secondary-drives" 2> /dev/null
+                        xdg-open $documentation 2> /dev/null
                         ;;
                 esac
+
     fi
     exfat=$new_exfat
     ntfs=$new_ntfs
-
 
 done
