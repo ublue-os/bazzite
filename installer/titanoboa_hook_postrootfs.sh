e96277624047c5f22cab97c0064403dec232b17c
kressb-gitlab <kressb@moose.net>
2026-01-23 09:52:06 -0500

    Change gui_post_login.sh to not just immediately exit on EFI systems (#3991)
    * Change gui_post_login.sh to not just immediately exit on EFI systems
    
    * Add missing space

--- installer/titanoboa_hook_postrootfs.sh
+++ installer/titanoboa_hook_postrootfs.sh
@@ -440,13 +440,11 @@ chmod +x /usr/bin/on_gui_login.sh
 mkdir -p /etc/skel/.config/autostart
 cat >>/usr/bin/on_gui_login.sh <<'EOF'
 # if CSM/Legacy show blocking message and power off
-if [ -d /sys/firmware/efi ]; then
-    exit 0
+if [[ ! -d /sys/firmware/efi ]]; then
+    yad --undecorated --on-top --timeout=0 --button=Shutdown:0 \
+        --text="Bazzite does not support CSM/Legacy Boot. Please boot into your UEFI/BIOS settings, disable CSM/Legacy Mode, and reboot." || true
+    systemctl poweroff || shutdown -h now || true
 fi
-yad --undecorated --on-top --timeout=0 --button=Shutdown:0 \
-    --text="Bazzite does not support CSM/Legacy Boot. Please boot into your UEFI/BIOS settings, disable CSM/Legacy Mode, and reboot." || true
-systemctl poweroff || shutdown -h now || true
-exit 0
 EOF
 
 cat >/etc/skel/.config/autostart/on_gui_login.desktop <<'EOF'
